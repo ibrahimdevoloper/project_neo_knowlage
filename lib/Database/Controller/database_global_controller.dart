52f2cc604818663a8a5efedbfe2ae418fa667218
Ibrahimdeveloper <ibrahimdevoloper@gmail.com>
2021-12-24 19:11:38 +0200

    Create database_global_controller.dart

---
+++ lib/Database/Controller/database_global_controller.dart
@@ -0,0 +1,16 @@
+import 'package:get/get.dart';
+import 'package:project_neo_knowlage/Database/database.dart';
+
+
+class DatabaseGlobalController extends GetxController{
+
+  AppDatabase? database;
+  static final databaseName="pokemonCards";
+
+  DatabaseGlobalController(this.database);
+
+  ///static function to initialize the local database
+  static Future<AppDatabase> getDatabase() async {
+    return await $FloorAppDatabase.databaseBuilder('$databaseName.db').build();
+  }
+}
