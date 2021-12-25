4f5124876819185cbeb2316db8e6aaf82d359905
Ibrahimdeveloper <ibrahimdevoloper@gmail.com>
2021-12-25 21:50:13 +0200

    Update view.dart

--- lib/Features/couches/view.dart
+++ lib/Features/couches/view.dart
@@ -6,8 +6,19 @@ import 'controller.dart';
 class CouchesPage extends StatelessWidget {
   final CouchesController controller = Get.put(CouchesController());
 
+  CouchesPage({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Couches")
+      ),
+      body: ListView.builder(itemBuilder: (context,i){
+        return ListTile(
+          title: Text("$i"),
+        );
+      }),
+    );
   }
 }
